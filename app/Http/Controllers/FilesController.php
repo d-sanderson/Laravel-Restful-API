419868a8ccf10c39cf5d4fefe453fc183adff1fb
dsanderson90 <dsanderson3@cnm.edu>
2019-04-17 21:27:30 -0600

    addded basic loggin middleware.

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -9,4 +9,9 @@ class FilesController extends Controller
     public function show() {
         return response()->download(storage_path('app/GraceHopper.pdf'), 'Amazing Grace');
     }
+
+    public function create(Request $request) {
+        $path = $request->file('photo')->store('testing');
+        return response()->json(['path' => $path], 200);
+    }
 }
