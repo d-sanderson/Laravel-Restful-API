a228e7019281c3ecfbfbaee8a2455f3d418dcf54
dsanderson90 <dsanderson3@cnm.edu>
2019-04-17 19:22:09 -0600

    created crud api and tested it using insomnia

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -4,10 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Poll;
+use Validator;
 
 class PollsController extends Controller
 {
     public function index() {
-        return response()->json(Poll::get(), 200);;
+        return response()->json(Poll::get(), 200);
     }
+
+    public function show($id) {
+        $poll = Poll::find($id);
+        if(is_null($poll)) {
+            return response()->json('POLL NOT FOUND 404', 404);
+        }
+        return response()->json(Poll::findOrFail($id), 200);
+    }
+
+    public function store(Request $request) {
+        $rules = [
+            "title" => 'required|max:255'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 404);
+        }
+        $poll = Poll::create($request->all());
+        return response()->json($poll, 201);
+    }
+
+    public function update(Request $request, Poll $poll) {
+        $poll->update($request->all());
+        return response()->json($poll, 200);
+    }
+
+    public function delete(Request $request, Poll $poll) {
+        $poll->delete();
+        return response()->json(null, 204);
+    }
+
+
 }
+
+
